303d52409a716ff6b6d086545f7d6f496dae6df1
solomonope <solomonope@gmail.com>
2018-07-23 02:52:58 +0200

    Add forward pass of alexnet pytorch

--- alexnet/pytorch.py
+++ alexnet/pytorch.py
@@ -1,49 +1,60 @@
 import torch
-
-'''
- import torch.nn as nn
-    import torch.nn.functional as F
-
-    class Model(nn.Module):
-        def __init__(self):
-            super(Model, self).__init__()
-            self.conv1 = nn.Conv2d(1, 20, 5)
-            self.conv2 = nn.Conv2d(20, 20, 5)
-
-        def forward(self, x):
-            x = F.relu(self.conv1(x))
-            
-            return F.relu(self.conv2(x))
-
-'''
+import torch.nn.functional as F
 
 
 class Alexnet(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, num_of_classes):
         super(Alexnet, self).__init__()
         self.conv_1 = torch.nn.Conv2d(3, 96, 11, stride=4)
-
         self.pool_1 = torch.nn.MaxPool2d(3, stride=2)
 
-        conv_2 = tf.keras.layers.Conv2D(256, kernel_size=(5, 5), activation="relu")(norm_1)
-        pool_2 = tf.keras.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(conv_2)
-        norm_2 = tf.keras.layers.BatchNormalization()(pool_2)
+        self.conv_2 = torch.nn.Conv2d(96, 256, 5)
+        self.pool_2 = torch.nn.MaxPooling2d(pool_size=(3, 3), strides=(2, 2))
 
-        conv_3 = tf.keras.layers.Conv2D(256, kernel_size=(3, 3), activation="relu")(norm_2)
-        conv_4 = tf.keras.layers.Conv2D(384, kernel_size=(3, 3), activation="relu")(conv_3)
-        conv_5 = tf.keras.layers.Conv2D(384, kernel_size=(3, 3), activation="relu")(conv_4)
+        self.conv_3 = torch.nn.Conv2d(256, kernel_size=(3, 3))
+        self.conv_4 = torch.nn.Conv2d(384, kernel_size=(3, 3))
+        self.conv_5 = torch.nn.Conv2d(384, kernel_size=(3, 3))
 
-        pool_3 = tf.keras.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(conv_5)
-        norm_3 = tf.keras.layers.BatchNormalization()(pool_3)
+        self.pool_3 = torch.nn.MaxPooling2d(pool_size=(3, 3), strides=(2, 2))
 
-        flat1 = tf.keras.layers.Flatten()(norm_3)
-        dense_1 = tf.keras.layers.Dense(4096, activation="tanh")(flat1)
-        dropout_1 = tf.keras.layers.Dropout(0.5)(dense_1)
+        self.dense_1 = torch.nn.Linear(384, 4096)
+        self.dropout_1 = torch.nn.Dropout(0.5)
 
-        dense_2 = tf.keras.layers.Dense(4096, activation="tanh")(dropout_1)
-        dense_3 = tf.keras.layers.Dropout(0.5)(dense_2)
+        self.dense_2 = torch.nn.Linear(4096, 4096)
+        self.dropout_2 = torch.nn.Dropout(0.5)
 
-        output = tf.keras.layers.Dense(17, activation="softmax")(dense_3)
+        self.output = torch.nn.Linear(4096, num_of_classes)
 
-        self.model = tf.keras.Model(inputs=input, outputs=output)
-        self.model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=["accuracy"])
+    def forward(self, *input):
+        x = self.conv_1(input)
+        x = F.relu(x)
+        x = self.pool_1(x)
+
+        x = self.conv_2(x)
+        x = F.relu(x)
+        x = self.pool_2(x)
+
+        x = self.conv_3(x)
+        x = F.relu(x)
+        x = self.conv_4(x)
+        x = F.relu(x)
+        x = self.conv_5(x)
+        x = self.pool_3(x)
+
+        x = x.view(x.size(0), -1)
+
+        x = self.dense_1(x)
+        x = self.dropout_1(x)
+
+        x = self.dense_2(x)
+        x = self.dropout_2(x)
+        return F.softmax(self.output(x))
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def evaluate(self):
+        pass
